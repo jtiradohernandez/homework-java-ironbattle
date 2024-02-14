952f8db9b17867737c0cd307fc97e67f4e053da9
evagapi <87602505+evagapi@users.noreply.github.com>
2024-02-14 16:08:13 +0100

    refactor: move battle logs to Bard class

---
+++ src/Bard.java
@@ -0,0 +1,30 @@
+public class Bard {
+    public void introducesStory() {
+        System.out.println("Welcome to the Binary Realms.\n" +
+                "In a land where legends are forged and destinies entwined.\n" +
+                "Amidst the dappled shadows, beneath the canopy's verdant embrace, two figures emerge.\n" +
+                "Their resolve as unyielding as the ancient oaks that watch over their battle.\n");
+    }
+    public void introducesFirstOpponent(String firstOpponentName) {
+        System.out.printf("Amidst the swirling mists of fate %s emerges, a harbinger of challenge and strife.\n", firstOpponentName);
+    }
+    public void introducesSecondOpponent(String secondOpponentName) {
+        System.out.printf("And so does %s, a shadow cast from the depths of adversity.\n", secondOpponentName);
+    }
+    public void announcesRound(int roundNumber) {
+        System.out.printf("=========== ROUND %d ===========\n", roundNumber);
+    }
+    public void narratesAttack(String opponentName) {
+        System.out.printf("%s attacks\n", opponentName);
+    }
+    public void announcesOpponentDeath(String opponentName) {
+        System.out.printf("Alas, the noble %s succumbs to fate's cruel embrace and dies.\n", opponentName);
+    }
+    public void announcesOpponentVictory(String opponentName) {
+        System.out.printf("The valiant %s shatters the foe, claiming victory in this epic duel!", opponentName);
+    }
+    public void announcesTie() {
+        System.out.println("The battle's crescendo yields naught but a resolute tie.\n" +
+                "Once more, the clash of titans echoes across the realm, as the cycle of battle begins anew.\n");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,10 @@ public class Main {
         //Temporary creation of characters, can be deleted
         Character char1 = new Warrior("guerrero",180,12,2);
         Character char2  = new Wizard("wizard",100,12,2);
+        Bard bard = new Bard();
 
         //Logs (as storytelling) begin
-        System.out.println("Welcome to the Binary Realms.\n");
-        System.out.println("In a land where legends are forged and destinies entwined.\n");
-        System.out.println("Amidst the dappled shadows, beneath the canopy's verdant embrace, two figures emerge.\n");
-        System.out.println("Their resolve as unyielding as the ancient oaks that watch over their battle.\n");
+        bard.introducesStory();
         //Call for the method that will do the combat
         combat(char1,char2);
     }
@@ -22,49 +20,49 @@ public class Main {
 
         boolean finished = false;
         int roundCounter = 1;
+        Bard bard = new Bard();
 
         Character initChar1 = char1.clone();
-        System.out.printf("Amidst the swirling mists of fate %s emerges, a harbinger of challenge and strife.", initChar1.getName());
+        bard.introducesFirstOpponent(initChar1.getName());
 
         Character initChar2 = char2.clone();
-        System.out.printf("And so does %s, a shadow cast from the depths of adversity.", initChar2.getName());
+        bard.introducesSecondOpponent(initChar2.getName());
 
         //Loop until one of the character wins
         while(!finished){
 
             //Loop for the battle
-            while(char1.isAlive()&&char2.isAlive()){
-                System.out.printf("=========== ROUND %d =========== \n", roundCounter);
+            while(char1.isAlive() && char2.isAlive()){
+                bard.announcesRound(roundCounter);
                 roundCounter += 1;
                 char1.attack(char2);
-                System.out.println(char1.getName() + " attacks.");
+                bard.narratesAttack(char1.getName());
                 char2.attack(char1);
-                System.out.println(char2.getName() + " attacks.");
+                bard.narratesAttack(char2.getName());
                 if (char1.getHp() <= 0) {
                     char1.setAlive(false);
-                    System.out.println(char1.getName() + " dies.");
+                    bard.announcesOpponentDeath(char1.getName());
                 }
                 if (char2.getHp() <= 0) {
                     char2.setAlive(false);
-                    System.out.println(char2.getName() + " dies.");
+                    bard.announcesOpponentDeath(char2.getName());
                 }
             }
 
             //Once the battle is finished, let's check who wins the battle
             if (!char2.isAlive()&&!char1.isAlive()){
                 //The battle result is a tie
-                System.out.println("The battle's crescendo yields naught but a resolute tie.");
-                System.out.println("Once more, the clash of titans echoes across the realm, as the cycle of battle begins anew.");
+                bard.announcesTie();
                 char1=initChar1.clone();
                 char2=initChar2.clone();
 
             }else if (!char2.isAlive()){
                 //Character 2 wins the battle
-                System.out.printf("The valiant %s shatters the foe, claiming victory in this epic duel!", char1.getName());
+                bard.announcesOpponentVictory(char1.getName());
                 finished = true;
             }else if(!char1.isAlive()){
                 //Character 1 wins the battle
-                System.out.printf("The valiant %s shatters the foe, claiming victory in this epic duel!", char2.getName());
+                bard.announcesOpponentVictory(char2.getName());
                 finished = true;
             }
         }
