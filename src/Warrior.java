88adb79e0521c5f16ce921c9051f1af3ab0bf74b
katalinjordans <katalinjordans@gmail.com>
2024-02-17 11:13:22 +0100

    Replaced MIN/MAX variables with RANGE

--- src/Warrior.java
+++ src/Warrior.java
@@ -3,29 +3,26 @@ import java.util.Random;
 public class Warrior extends Character {
     private int stamina;
     private int strength;
-    private final int STAMINA_MIN = 10;
-    private final int STAMINA_MAX = 50;
-    private final int STRENGTH_MIN = 1;
-    private final int STRENGTH_MAX = 10;
-    private final int HP_MIN = 100;
-    private final int HP_MAX = 200;
+    private static final int[] STAMINA_RANGE = {10, 50};
+    private final int[] STRENGTH_RANGE = {1, 10};
+    private final int[] HP_RANGE = {100, 200};
     private final int HEAVY_ATTACK_STAMINA = 5;
     private static final String[] warriorsNames = new String[]{"Aragorn", "Xena", "Conan", "Mulán", "Wonder Woman", "Samurai Jack", "Thor", "Brienne of Tarth", "Leonidas", "Boudica", "Legolas", "Joan of Arc", "Beowulf", "Attila the Hun", "Cleopatra", "Genghis Khan", "Hua Mulan", "William Wallace", "Ragnar Lothbrok", "Sun Tzu", "Zaraki Kenpachi", "Khal Drogo", "King Arthur", "Spartacus", "Achilles", "Okoye", "Captain America", "Black Panther"};
 
     public Warrior(String name, int hp, int stamina, int strength) {
         super(name);
-        if (hp < HP_MIN || hp > HP_MAX) {
-            throw new IllegalArgumentException("HP must be between " + HP_MIN + " and " + HP_MAX);
+        if (hp < HP_RANGE[0] || hp > HP_RANGE[1]) {
+            throw new IllegalArgumentException("HP must be between " + HP_RANGE[0] + " and " + HP_RANGE[1]);
         } else {
             setHp(hp);
         }
-        if (stamina < STAMINA_MIN || stamina > STAMINA_MAX) {
-            throw new IllegalArgumentException("Stamina must be between " + STAMINA_MIN + " and " + STAMINA_MAX);
+        if (stamina < STAMINA_RANGE[0] || stamina > STAMINA_RANGE[1]) {
+            throw new IllegalArgumentException("Stamina must be between " + STAMINA_RANGE[0] + " and " + STAMINA_RANGE[1]);
         } else {
             setStamina(stamina);
         }
-        if (strength < STRENGTH_MIN || strength > STRENGTH_MAX) {
-            throw new IllegalArgumentException("Strength must be between " + STRENGTH_MIN + " and " + STRENGTH_MAX);
+        if (strength < STRENGTH_RANGE[0] || strength > STRENGTH_RANGE[1]) {
+            throw new IllegalArgumentException("Strength must be between " + STRENGTH_RANGE[0] + " and " + STRENGTH_RANGE[1]);
         } else {
             setStrength(strength);
         }
@@ -34,9 +31,9 @@ public class Warrior extends Character {
 
     public Warrior() {
         super(randomName());
-        setHp(randomInt(HP_MIN, HP_MAX));
-        setStamina(randomInt(STAMINA_MIN, STAMINA_MAX));
-        setStrength(randomInt(STRENGTH_MIN, STRENGTH_MAX));
+        setHp(randomInt(HP_RANGE[0], HP_RANGE[1]));
+        setStamina(randomInt(STAMINA_RANGE[0], STAMINA_RANGE[1]));
+        setStrength(randomInt(STRENGTH_RANGE[0], STRENGTH_RANGE[1]));
     }
 
     public int getStamina() {
