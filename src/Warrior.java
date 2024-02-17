f15c6893bc1cab86237150a26c218f32007be597
katalinjordans <katalinjordans@gmail.com>
2024-02-17 10:32:24 +0100

    refactoring including update of min/max variables in warrior class to range, removing of redundant setHp override method, adding error messages in wizard class if variables are set outside the allowed range, addition of attack method in character class to be overridden in wizard and warrior classes

--- src/Warrior.java
+++ src/Warrior.java
@@ -1,20 +1,39 @@
-// A Warrior is a Character that fights by attacking. Attacks inflict damage and in order to do one,
-// stamina is required.
+import java.util.Random;
+
 public class Warrior extends Character {
     private int stamina;
     private int strength;
-    private final int STAMINA_MIN = 10;
-    private final int STAMINA_MAX = 50;
-    private final int STRENGTH_MIN = 1;
-    private final int STRENGTH_MAX = 10;
-    private final int HP_MIN = 100;
-    private final int HP_MAX = 200;
+    private static final int[] STAMINA_RANGE = {10, 50};
+    private final int[] STRENGTH_RANGE = {1, 10};
+    private final int[] HP_RANGE = {100, 200};
     private final int HEAVY_ATTACK_STAMINA = 5;
+    private static final String[] warriorsNames = new String[]{"Aragorn", "Xena", "Conan", "Mulán", "Wonder Woman", "Samurai Jack", "Thor", "Brienne of Tarth", "Leonidas", "Boudica", "Legolas", "Joan of Arc", "Beowulf", "Attila the Hun", "Cleopatra", "Genghis Khan", "Hua Mulan", "William Wallace", "Ragnar Lothbrok", "Sun Tzu", "Zaraki Kenpachi", "Khal Drogo", "King Arthur", "Spartacus", "Achilles", "Okoye", "Captain America", "Black Panther"};
 
     public Warrior(String name, int hp, int stamina, int strength) {
-        super(name, hp);
-        setStamina(stamina);
-        setStrength(strength);
+        super(name);
+        if (hp < HP_RANGE[0] || hp > HP_RANGE[1]) {
+            throw new IllegalArgumentException("HP must be between " + HP_RANGE[0] + " and " + HP_RANGE[1]);
+        } else {
+            setHp(hp);
+        }
+        if (stamina < STAMINA_RANGE[0] || stamina > STAMINA_RANGE[1]) {
+            throw new IllegalArgumentException("Stamina must be between " + STAMINA_RANGE[0] + " and " + STAMINA_RANGE[1]);
+        } else {
+            setStamina(stamina);
+        }
+        if (strength < STRENGTH_RANGE[0] || strength > STRENGTH_RANGE[1]) {
+            throw new IllegalArgumentException("Strength must be between " + STRENGTH_RANGE[0] + " and " + STRENGTH_RANGE[1]);
+        } else {
+            setStrength(strength);
+        }
+        setCharacterClass("warrior");
+    }
+
+    public Warrior() {
+        super(randomName());
+        setHp(randomInt(HP_RANGE[0], HP_RANGE[1]));
+        setStamina(randomInt(STAMINA_RANGE[0], STAMINA_RANGE[1]));
+        setStrength(randomInt(STRENGTH_RANGE[0], STRENGTH_RANGE[1]));
     }
 
     public int getStamina() {
@@ -22,11 +41,7 @@ public class Warrior extends Character {
     }
 
     public void setStamina(int stamina) {
-        if(stamina < STAMINA_MIN || stamina > STAMINA_MAX) {
-            throw new IllegalArgumentException("Stamina must be between " + STAMINA_MIN + " and " + STAMINA_MAX);
-        } else {
-            this.stamina = stamina;
-        }
+        this.stamina = stamina;
     }
 
     public int getStrength() {
@@ -34,25 +49,14 @@ public class Warrior extends Character {
     }
 
     public void setStrength(int strength) {
-        if(strength < STRENGTH_MIN || strength > STRENGTH_MAX) {
-            throw new IllegalArgumentException("Strength must be between " + STRENGTH_MIN + " and " + STRENGTH_MAX);
-        } else {
-            this.strength = strength;
-        }
-    }
-
-    public void setHp(int hp) {
-        if(hp < HP_MIN || hp > HP_MAX) {
-            throw new IllegalArgumentException("HP must be between " + HP_MIN + " and " + HP_MAX);
-        } else {
-            super.setHp(hp);
-        }
+        this.strength = strength;
     }
 
     @Override
     public void attack(Character character) {
         if (getStamina() == 0) {
-            setStamina(getStamina()+2);
+            setStamina(getStamina() + 2);
+            Bard.narratesRest(this);
         } else if (getStamina() < HEAVY_ATTACK_STAMINA) {
             weakAttack(character);
         } else {
@@ -66,16 +70,30 @@ public class Warrior extends Character {
         }
     }
 
+    @Override
+    public Character clone() {
+        return new Warrior(getName(), getHp(), getStamina(), getStrength());
+    }
+
     public void heavyAttack(Character character) {
-        setStamina(getStamina()-HEAVY_ATTACK_STAMINA);
-        character.setHp(character.getHp()-getStrength());
+        final int HP_LOSS = getStrength();
+
+        setStamina(getStamina() - HEAVY_ATTACK_STAMINA);
+        character.setHp(character.getHp() - HP_LOSS);
+        Bard.narratesAttack(this, "launches a heavy attack ⚔\uFE0F", HP_LOSS);
     }
 
     public void weakAttack(Character character) {
         final int STAMINA_RECOVER = 1;
-        final int HP_LOSS = getStrength()/2;
+        final int HP_LOSS = getStrength() / 2;
 
-        setStamina(getStamina()+STAMINA_RECOVER);
-        character.setHp(character.getHp()-HP_LOSS);
+        setStamina(getStamina() + STAMINA_RECOVER);
+        character.setHp(character.getHp() - HP_LOSS);
+        Bard.narratesAttack(this, "launches a weak attack \uD83D\uDD2A", HP_LOSS);
+    }
+
+    public static String randomName() {
+        Random random = new Random();
+        return warriorsNames[random.nextInt(warriorsNames.length)];
     }
 }
--- src/Wizard.java
+++ src/Wizard.java
@@ -1,15 +1,30 @@
+import java.util.Random;
+
 public class Wizard extends Character {
     private int mana;
     private int intelligence;
+    private static final int[] HP_RANGE = {50, 100};
     private final int[] MANA_RANGE = {10, 50};
     private final int[] INTELLIGENCE_RANGE = {1, 50};
-
     private final int FIREBALL_MANA = 5;
+    private static final String[] wizardsNames = new String[]{"Merlin", "Gandalf", "Severus", "Alatar", "Dumbledore", "Rincewind", "Voldemort", "Albus", "Asterope", "Astra", "Atlantes", "Beatrix", "Belinda", "Fawley", "Glinda", "Gwydion", "Jadis", "Jareth", "Morgan", "Potter", "Prospero", "Radagast", "Saruman", "Thoth-Amon"};
 
     public Wizard(String name, int hp, int mana, int intelligence) {
-        super(name, hp); //TODO: check hp range (50-100)
+        super(name, hp);
+        if (hp < HP_RANGE[0] || hp > HP_RANGE[1]) {
+            throw new IllegalArgumentException("HP must be between " + HP_RANGE[0] + " and " + HP_RANGE[1]);
+        } else {
+            setHp(hp);
+        }
         setMana(mana);
         setIntelligence(intelligence);
+        setCharacterClass("wizard");
+    }
+
+    public Wizard() {
+        super(randomName(), randomInt(HP_RANGE[0], HP_RANGE[1]));
+        setMana(randomInt(MANA_RANGE[0], MANA_RANGE[1]));
+        setIntelligence(randomInt(INTELLIGENCE_RANGE[0], INTELLIGENCE_RANGE[1]));
     }
 
     public int getMana() {
@@ -21,23 +36,25 @@ public class Wizard extends Character {
     }
 
     public void setMana(int mana) {
-        if (mana >= MANA_RANGE[0] && mana <= MANA_RANGE[1]) {
+        if (mana < MANA_RANGE[0] || mana > MANA_RANGE[1]) {
+            throw new IllegalArgumentException("Mana must be between " + MANA_RANGE[0] + " and " + MANA_RANGE[1]);
+        } else {
             this.mana = mana;
         }
-        //TODO: if number is not in the range, call new random number
     }
 
     public void setIntelligence(int intelligence) {
-        if (intelligence >= INTELLIGENCE_RANGE[0] && mana <= INTELLIGENCE_RANGE[1]) {
+        if (intelligence < INTELLIGENCE_RANGE[0] || intelligence > INTELLIGENCE_RANGE[1]) {
+            throw new IllegalArgumentException("Intelligence must be between " + INTELLIGENCE_RANGE[0] + " and " + INTELLIGENCE_RANGE[1]);
+        } else {
             this.intelligence = intelligence;
         }
-        //TODO: if number is not in the range, call new random number
     }
 
     @Override
     public void attack(Character character) {
         if (getMana() == 0) {
-            setMana(getMana()+2);
+            setMana(getMana() + 2);
         } else if (getMana() < FIREBALL_MANA) {
             staffHit(character);
         } else {
@@ -51,16 +68,30 @@ public class Wizard extends Character {
         }
     }
 
+    @Override
+    public Character clone() {
+        return new Wizard(getName(), getHp(), getMana(), getIntelligence());
+    }
+
     private void fireball(Character character) {
-        setMana(getMana()-FIREBALL_MANA);
-        character.setHp(character.getHp()-getIntelligence());
+        final int HP_LOSS = getIntelligence();
+        setMana(getMana() - FIREBALL_MANA);
+        character.setHp(character.getHp() - HP_LOSS);
+        Bard.narratesAttack(this, "casts a fireball \uD83D\uDD25", HP_LOSS);
     }
 
     private void staffHit(Character character) {
         final int MANA_RECOVER = 1;
         final int HP_LOSS = 2;
 
-        setMana(getMana()+MANA_RECOVER);
-        character.setHp(character.getHp()-HP_LOSS);
+        setMana(getMana() + MANA_RECOVER);
+        character.setHp(character.getHp() - HP_LOSS);
+        Bard.narratesAttack(this, "executes a staff hit \uD83E\uDDF9", HP_LOSS);
+    }
+
+
+    public static String randomName() {
+        Random random = new Random();
+        return wizardsNames[random.nextInt(wizardsNames.length)];
     }
 }
